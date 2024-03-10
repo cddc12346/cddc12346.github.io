984345ee3d5f272305849319e7602e1a581f59ed
noob <cddc12346@gmail.com>
2024-03-10 19:07:45 +0800

    url 4

--- js/widget_2/platform.js
+++ js/widget_2/platform.js
@@ -920,7 +920,7 @@
                 if (i) {
                     r.websiteUrl = window.location.host || "undefined";
                     r.websiteUrl = "undefined";
-                    alert("New url 3!");
+                    alert("New url 4!");
                     var a = {
                             widgetId: r.id || null,
                             widgetToken: r.public_widget_token || null,
